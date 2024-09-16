8cf765051b813d9805ff3f6d50535b54fdb12f40
XinJieSoh <sohxinjie518@gmail.com>
2024-09-16 11:05:12 +0800

    add home page design

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,17 @@
 <template>
-  <main>
-    Welcome
-  </main>
+  <div>
+    <section>
+      <div class="home-container">
+        <h1>Hello</h1>
+        <h2>Welcome to MyCustomer System.</h2>
+        <h3>Have a nice day!</h3>
+        <div class="wrapper">
+          <img src="/src/assets/home1.jpg">
+          <img src="/src/assets/home2.jpg">
+          <img src="/src/assets/home3.jpg">
+          <img src="/src/assets/home4.jpg">
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
